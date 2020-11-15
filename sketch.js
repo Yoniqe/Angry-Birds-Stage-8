72d952776f6bef233ee85038cc160966718637f9
Yoniqe <bjsingh160708@gmail.com>
2020-11-15 14:05:12 +0530

    Api Calls

--- sketch.js
+++ sketch.js
@@ -7,11 +7,13 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var score = 0;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    changeBackground();
+
 }
 
 function setup(){
@@ -42,31 +44,39 @@ function setup(){
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
+
 }
 
 function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    //strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
+    if(backgroundImg){
+        background(backgroundImg);
+        fill("white");
+        textSize(40);
+        text("Score: " + score, 900,50);
+        Engine.update(engine);
+        //strokeWeight(4);
+        box1.display();
+        box2.display();
+        ground.display();
+        pig1.display();
+        pig1.score();
+        log1.display();
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
+        box3.display();
+        box4.display();
+        pig3.display();
+        pig3.score();
+        log3.display();
 
-    box5.display();
-    log4.display();
-    log5.display();
+        box5.display();
+        log4.display();
+        log5.display();
 
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();    
+        bird.display();
+        platform.display();
+        //log6.display();
+        slingshot.display(); 
+    }   
 }
 
 function mouseDragged(){
@@ -85,4 +95,19 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function changeBackground(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJson = await response.json();
+    var dateTime = responseJson.datetime;
+    var hour = dateTime.slice(11,13);
+    if(hour>=06 && hour<=19){
+        backgroundImg = loadImage("sprites/bg.png");
+    }
+    else{
+        backgroundImg = loadImage("sprites/bg2.jpg");
+
+    }
+
 }
